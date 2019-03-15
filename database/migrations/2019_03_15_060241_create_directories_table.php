fe7ae5778e1ad64b5274d14d8648d22b57e35b2c
rileymanda1@gmail.com <rileymanda1@gmail.com>
2019-03-15 09:05:24 +0300

    created model & migrations

---
+++ database/migrations/2019_03_15_060241_create_directories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDirectoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('directories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('home_number');
+            $table->string('mobile_number');
+            $table->string('email_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('directories');
+    }
+}
